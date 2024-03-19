3b4b9815ae812de6fb9ce0c26ba5a55a93711887
facutolozadev <facutoloza2005@gmail.com>
2024-03-19 08:47:12 -0300

    fix: quit base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://facutolozadev.github.io/',
-  base: 'https://github.com/facutolozadev/facutolozadev.github.io.git'
+  site: 'https://facutolozadev.github.io/'
 });
